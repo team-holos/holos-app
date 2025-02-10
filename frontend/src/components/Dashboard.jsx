3ba734696240922b77c2cd2e6ecfe8134872865e
Yasemin Ceylan <yaceybln@gmail.com>
2025-02-10 12:28:00 +0100

    added component to dashboard

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
 
 function Dashboard() {
-    
-<h1>Welcome to the Dashboard</h1>
+    return (
 
+    
+
+    <div>
+        <ul>
+            <li>
+                <h1>Welcome User!</h1>
+            </li>
+            <li>Tagesübersicht</li>
+            <li>Ziel für heute</li>
+        </ul>
+        <form>
+            <label for="Shortcuts">"Menu"</label>
+            <select id="übersicht" name="Übersicht">
+            <option value="ernährung" name="Ernährung">Ernährung</option>
+            <option value="fitness" name="Fitness">Fitness</option>
+            <option value="mentale Gesundheit" name="Mentale Gesundheit">Mentale Gesundheit</option>
+            <option value="entspannung" name="Entspannung">Entspannung</option>
+            </select>
+        </form>
+    </div>
+);
 }
 
 

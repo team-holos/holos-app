2fea2c5d2994e6530d1625b853d097b5460acff5
stephipie <stephi.pietschmann@gmail.com>
2025-02-10 11:09:25 +0100

    add components folder and start with Dahboard component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./components/Dashboard";
+
+
+function App() {
+    return (
+        <h1>Hello World</h1>
+    );
+}
+
+
+export default App;
---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function Dashboard() {
+<h1>Welcome to the Dashboard</h1>
+
+}
+
+
+export default Dashboard;

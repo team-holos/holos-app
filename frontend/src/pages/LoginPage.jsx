580a3cb57f01fdb6f48af57fb617fa2960090715
affan14 <affan.ahmad@yahoo.de>
2025-02-11 15:31:23 +0100

    Added import Link from react-router-dom

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import useAuthStore from "../components/store/authstore";
+import { Link } from "react-router-dom";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 const API_URL = import.meta.env.VITE_API_URL;
+import { Link } from "react-router-dom";
 
 function RegisterPage() {
   const [emailError, setEmailError] = useState(null);

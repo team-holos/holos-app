74949b34ffe8eb94f7c2aac4bcda90ebf18aa042
VinJust <vpj97@outlook.de>
2025-02-11 12:35:11 +0100

    Added AIChat route to App.jsx and BrowserRouter now wraps around Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
+import AIChat from "./components/AIChat";
 import Layout from "./layout/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
-    return (
-        
-        <Layout>
-               <BrowserRouter>
-                  <Routes>
-                    <Route path="/" element={<DashboardPage />} />
-                  </Routes>
-                </BrowserRouter>
-        </Layout>
-    );
-};
-
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/chat" element={<AIChat />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
 
 export default App;

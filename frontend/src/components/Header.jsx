c9d906f88aee05e2895d83b9d1cc565f41a7f14c
stephipie <stephi.pietschmann@gmail.com>
2025-02-11 16:06:07 +0100

    change style form header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,9 +3,11 @@ import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <header className="bg-[#7886C7] flex items-center justify-between py-4 mx-4 rounded">
-      <h1 className="text-[#FFF2F2]">Holos App</h1>
+    <header className="bg-[#7886C7] flex width-full items-center justify-between rounded">
+      <h1 className="text-[#FFF2F2] py-4 mx-4">Holos App</h1>
+      <div className="pr-4">
       <Navigation />  
+      </div>
     </header>
   );
 }

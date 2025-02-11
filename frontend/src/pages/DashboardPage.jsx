94329111c0f3a731d36ef646984f2a6ee2e785d7
stephipie <stephi.pietschmann@gmail.com>
2025-02-11 11:43:52 +0100

    install lucide-react to add burger icon

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
-import Nutrition from "./NutritionPage";
-import { Link, useNavigate } from "react-router-dom";
+// import { useState } from "react";
+// import Nutrition from "./NutritionPage";
+import { useNavigate } from "react-router-dom";
+import { Menu } from "lucide-react";
+
 function DashboardPage() {
     const navigate = useNavigate();
     const handleNavigation = (event) => {
@@ -19,11 +21,11 @@ function DashboardPage() {
             </ul>
 
 
-            <form>
-                <label htmlFor="shortcuts">Menu</label>
+            <form className="flex items-center">
+                <label htmlFor="shortcuts" className="inline-block"><Menu /></label>
                 <select id="uebersicht" name="uebersicht" onChange={handleNavigation}>
-                    <option value="">Select a page...</option>
-                    <option value="/nutrion">Ernährung</option>
+                    {/* <option value="">Select a page...</option> */}
+                    <option value="/nutrion" className="inline-block">Ernährung</option>
                     <option value="/fitness">Fitness</option>
                     <option value="/mentalhealth">Mentale Gesundheit</option>
                     <option value="/relaxation">Entspannung</option>

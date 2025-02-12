f0a58db428c34fbcbc97664dbfbb64d0504bc4d1
stephipie <stephi.pietschmann@gmail.com>
2025-02-12 12:14:50 +0100

    add login and register to routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import MentalHealthPage from "./pages/MentalHealthPage";
 import NutritionPage from "./pages/NutritionPage";
 import RelaxationPage from "./pages/RelaxationPage";
 import SettingsPage from "./pages/SettingsPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
           <Route path="/nutrion" element={<NutritionPage />} />
           <Route path="/relaxation" element={<RelaxationPage />} />
           <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>

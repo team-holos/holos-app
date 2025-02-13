71615ce70dd1c203e04cbd74520448f74be6555b
stephipie <stephi.pietschmann@gmail.com>
2025-02-13 12:33:13 +0100

    add simple input form within relaxationpage and create a select moodform

---
+++ frontend/src/components/MoodForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function MoodForm() {
+    const [myMood, setMyMood] = useState("Erholt");
+  
+    const handleChange = (event) => {
+      setMyMood(event.target.value)
+    }
+  
+    return (
+      <form>
+        <select value={myMood} onChange={handleChange}>
+          <option value="Erholt">Erholt</option>
+          <option value="Energie">Voller Energie</option>
+          <option value="Müde">Müde</option>
+          <option value="Erschöpft">Noch erschöpft</option>
+        </select>
+      </form>
+    )
+  }
+
+  export default MoodForm;
--- frontend/src/pages/RelaxationPage.jsx
+++ frontend/src/pages/RelaxationPage.jsx
@@ -1,13 +1,51 @@
+import { useState } from "react";
+import MoodForm from "../components/MoodForm";
+
 function RelaxationPage() {
-    return (
-        <div className="text-[#2D336B] p-4 my-4">
-        <h1 className="text-2xl mb-4">Erholung</h1>
-        <ul className="list-disc list-inside">
-            <li>Smart-Alarm</li>
-            <li>Schlafanalyse-Dashboard</li>
-        </ul>
-        </div>
-    )
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  };
+  return (
+    <div className="text-[#2D336B] p-4 my-4">
+      <h1 className="text-2xl mb-4">Erholung</h1>
+      <ul className="list-disc list-inside">
+        <li>Smart-Alarm</li>
+        <li>Schlafanalyse-Dashboard</li>
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Einschlafzeit:
+          <input
+            type="number"
+            name="fallasleep"
+            value={inputs.fallasleep || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Aufwachzeit:
+          <input
+            type="number"
+            name="wakeuptime"
+            value={inputs.wakuptime || ""}
+            onChange={handleChange}
+          />
+        </label>
+
+        <input type="submit" />
+      </form>
+      <MoodForm />    
+    </div>
+  );
 }
 
 export default RelaxationPage;

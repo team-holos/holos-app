afa6e20c54f9458f23965f42aea9f5b1ff638b36
stephipie <stephi.pietschmann@gmail.com>
2025-02-14 12:28:48 +0100

    install use-sound and create sounds folder with  piano music

---
+++ frontend/src/components/SoundForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import useSound from 'use-sound';
+import piano from '../sounds/soft-piano.mp3'
+
+
+const SoundForm = () => {
+  const [playSound] = useSound({piano});
+
+  return (
+    <div>
+      <button onClick={playSound}>Play Sound</button>
+    </div>
+  );
+};
+
+export default SoundForm;

f369384c8a8f2163f74a2a2cc6b924628d9f45f6
stephipie <stephi.pietschmann@gmail.com>
2025-02-14 11:04:49 +0100

    add logic to meditation timer

--- frontend/src/pages/MentalHealthPage.jsx
+++ frontend/src/pages/MentalHealthPage.jsx
@@ -2,6 +2,9 @@ import MentalHealthForm from "../components/MentalHealthForm";
 import MyTimer from "../components/MyTimer";
 
 function MentalHealthPage() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 600); //10 minutes timer
+  
   return (
     <div className="text-[#2D336B] p-4 my-4">
       <h1 className="text-2xl mb-4">Mentale Gesundheit</h1>
@@ -10,7 +13,7 @@ function MentalHealthPage() {
         <MentalHealthForm />
      </div>
         <h3 className="mt-2 font-semibold">Meditations-Bereich</h3>
-      <MyTimer />
+      <MyTimer expiryTimestamp={time} />
     </div>
   );
 }

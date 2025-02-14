a1c1605dc461073cfcd843b0a2f76f9c7a0cfcca
Yasemin Ceylan <yaceybln@gmail.com>
2025-02-14 12:09:30 +0100

    set new color for text

--- frontend/src/pages/NutritionPage.jsx
+++ frontend/src/pages/NutritionPage.jsx
@@ -20,7 +20,7 @@ function NutritionPage() {
   const ausgewählterPlan = nutritionData.find(plan => plan.id === planId);
 
   return (
-    <div className="text-[#2D336B] p-4 my-4">
+    <div className="text-[#A9B5DF] p-4 my-4">
       <h1 className="text-2xl mb-4">Ernährung</h1>
 
       <select value={planId} onChange={e => setSelectedPlanId(parseInt(e.target.value, 10))}>

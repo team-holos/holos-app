99bc7ff9c1a2b29a05b6203092c9f257b4bfa029
stephipie <stephi.pietschmann@gmail.com>
2025-02-17 14:33:43 +0100

    downsize speichern button

--- frontend/src/components/MentalHealthForm.jsx
+++ frontend/src/components/MentalHealthForm.jsx
@@ -12,13 +12,13 @@ function MentalHealthForm() {
   };
 
   return (
-    <form>
+    <form >
       <textarea
         value={textarea}
         onChange={handleChange}
         className="border p-2 my-2 w-full rounded mt-4 font-extralight"
       />
-      <button onClick={handleSave} className="p-2 border rounded cursor-pointer  bg-[#2D336B] text-[#FFF2F2]">
+      <button onClick={handleSave} className="text-xs p-2 border rounded cursor-pointer  bg-[#2D336B] text-[#FFF2F2]">
         Speichern
       </button>
     </form>

a5faa310e4f4774afa3ea6d8cbea62aafcb8f2b3
stephipie <stephi.pietschmann@gmail.com>
2025-02-17 15:59:17 +0100

    set default value to textarea and clear it after user input and save

--- frontend/src/components/MentalHealthForm.jsx
+++ frontend/src/components/MentalHealthForm.jsx
@@ -1,24 +1,37 @@
 import { useState } from "react";
 
 function MentalHealthForm() {
-  const [textarea, setTextarea] = useState("Dein persönliches Journal....");
-  const handleSave = () => {
-    alert("Einstellungen gespeichert");
+  const defaultMessage = "Dein persönliches Journal....";
+  const [textarea, setTextarea] = useState(defaultMessage);
 
+  const handleSave = (event) => {
+    event.preventDefault();
+    alert("Einstellungen gespeichert");
+    setTextarea("");
   };
 
   const handleChange = (event) => {
-    setTextarea(event.target.value);
+    if (event.target.value === defaultMessage) {
+      setTextarea(event.target.value);
+    } else {
+      setTextarea(event.target.value);
+    }
   };
 
   return (
     <form>
       <textarea
         value={textarea}
+        onFocus={() => {if (textarea === defaultMessage) 
+          {setTextarea("");}
+          }}
         onChange={handleChange}
         className="border p-2 my-2 w-full rounded mt-4 font-extralight"
       />
-      <button onClick={handleSave} className="text-xs p-2 border rounded cursor-pointer  bg-[#2D336B] text-[#FFF2F2]">
+      <button
+        onClick={handleSave}
+        className="text-xs p-2 border rounded cursor-pointer bg-[#2D336B] text-[#FFF2F2]"
+      >
         Speichern
       </button>
     </form>

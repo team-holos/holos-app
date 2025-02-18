3f2da945cb15460c00ff1257b38acc99cb97aa37
VinJust <vpj97@outlook.de>
2025-02-18 09:43:55 +0100

    Push of current backend

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -28,7 +28,7 @@ function RegisterPage() {
       return;
     }
 
-    const response = await fetch(`${API_URL}auth/register`, {
+    const response = await fetch(`${API_URL}/auth/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",

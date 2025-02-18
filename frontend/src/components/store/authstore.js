45ff2c765d3d28f1b74a9b7c138d1f70ebcb6348
VinJust <vpj97@outlook.de>
2025-02-18 16:08:54 +0100

    Added backend connection to LoginPage.jsx

--- frontend/src/components/store/authstore.js
+++ frontend/src/components/store/authstore.js
@@ -2,10 +2,18 @@ import { create } from "zustand";
 
 const useAuthStore = create((set, get) => ({
   token: null,
-  setToken: (token) => set({ token }),
-  //isLoggedIn: () => !!get().token,
-  isLoggedIn: () => {return false},
-  clearToken: () => set({ token: null }),
+  setToken: (token) => {
+    localStorage.setItem("token", token);
+    set({ token });
+  },
+  isLoggedIn: () => !!get().token,
+  clearToken: () => {
+    localStorage.removeItem("token");
+    set({ token: null });
+  },
 }));
 
+useAuthStore.getState().setToken(localStorage.getItem("token"));
+
 export default useAuthStore;
+

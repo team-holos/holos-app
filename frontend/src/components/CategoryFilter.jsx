76896742c234f86589c7f107fe9a70051fe37af3
Yasemin Ceylan <yaceybln@gmail.com>
2025-02-19 11:19:17 +0100

    update to CategoryFilter to work dynamic in backend

--- frontend/src/components/CategoryFilter.jsx
+++ frontend/src/components/CategoryFilter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ onChange }) {
     return (
         <div>
             <label htmlFor="category">Kategorie:</label>
-            <select id="category">
+            <select id="category" onChange={e => onChange(e.target.value)}>
                 <option value="all">Alle</option>
                 <option value="legDay">Leg Day</option>
+                <option value="core">Core</option>
+                <option value="lowerBody">Lower Body</option>
                 <option value="upperBody">Upper Body</option>
                 <option value="fullBody">Full Body</option>
                 <option value="cardio">Cardio</option>

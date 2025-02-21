2b44525239219b014f16c06ed6cd01b15d1d2072
Yasemin Ceylan <yaceybln@gmail.com>
2025-02-21 12:04:57 +0100

    added new options

--- frontend/src/components/CategoryFilter.jsx
+++ frontend/src/components/CategoryFilter.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 
 function CategoryFilter({ onChange }) {
-  const categories = [...new Set(exercisesData.map(exercise => exercise.category))];
+    const categories = [...new Set(exercisesData.map(exercise => exercise.category))];
 
-  return (
-    <div className="mb-4">
-      <label htmlFor="category" className="block text-sm font-medium text-gray-700">Kategorie:</label>
-      <select
-        id="category"
-        onChange={e => onChange(e.target.value)}
-        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 sm:text-sm"
-      >
-        <option value="all">Alle</option>
-        {categories.map(category => (
-          <option key={category} value={category}>{category}</option>
-        ))}
-      </select>
-    </div>
-  );
+    return (
+        <div className="mb-4">
+            <label htmlFor="category" className="block text-sm font-medium text-gray-700">Kategorie:</label>
+            <select
+                id="category"
+                onChange={e => onChange(e.target.value)}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 sm:text-sm"
+            >
+                <option value="all">Alle</option>
+                <option value="legDay">Leg Day</option>
+                <option value="core">Core</option>
+                <option value="lowerBody">Lower Body</option>
+                <option value="upperBody">Upper Body</option>
+                <option value="fullBody">Full Body</option>
+                <option value="cardio">Cardio</option>
+                <option value="pilates">Pilates</option>
+                {categories.map(category => (
+                    <option key={category} value={category}>{category}</option>
+                ))}
+            </select>
+        </div>
+    );
 }
 
 export default CategoryFilter;

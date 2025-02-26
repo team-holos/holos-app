f2b42d4f927d0c3e9542bb5c5f2343a0c0790919
kalevlill <lill.kalev@gmail.com>
2025-02-26 11:53:34 +0100

    navigation bar is removed from the login and register pages at the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,19 +2,23 @@ import { useLocation } from "react-router-dom";
 import Navigation from "./Navigation";
 import useAuthStore from "./store/authstore";
 import Logout from "./Logout";
+
 function Header() {
   const { isLoggedIn } = useAuthStore();
   const location = useLocation();
-  const hideLogout = location.pathname === "/" || location.pathname === "/register";
+  const hideLogout = location.pathname === "/" || location.pathname === "/register" || location.pathname === "/";
+  const hideNavigation = location.pathname === "/register" || location.pathname === "/";
+
   return (
     <header className="bg-[#7886C7] flex w-full items-center justify-between rounded">
       <h1 className="text-[#FFF2F2] py-4 mx-4">Holos App</h1>
       <div className="flex items-center space-x-4">
-        {isLoggedIn && <Navigation />}
+        {isLoggedIn && !hideNavigation && <Navigation />}
         {isLoggedIn && !hideLogout && <Logout />}
       </div>
     </header>
   );
 }
+
 export default Header;
 

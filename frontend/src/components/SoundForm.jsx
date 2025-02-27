6051755fae8d24858105d9e174a797d16f42bbc2
stephipie <stephi.pietschmann@gmail.com>
2025-02-27 12:54:39 +0100

    add styles and meditation feature to mentalHealthPage

--- frontend/src/components/SoundForm.jsx
+++ frontend/src/components/SoundForm.jsx
@@ -8,8 +8,8 @@ const SoundForm = () => {
     // <div className="flex flex-col items-center text-xs">
       <div style={{ textAlign: "center" }}>
       <p className="mb-2 text-xs">Falls gewünscht, kannst Du Dir hier entspannte Piano Musik zu Deiner Meditation anmachen:</p>
-      <button onClick={playSound} className="p-2 border mr-2 text-xs rounded cursor-pointer  bg-[#2D336B] text-[#FFF2F2]">Play Sound</button>
-      <button onClick={() => stop()} className="p-2 border text-xs rounded cursor-pointer  bg-[#2D336B] text-[#FFF2F2]">Stop Sound</button>
+      <button onClick={playSound} className="p-2 border mr-2 text-xs rounded cursor-pointer  text-white bg-[#7886C7] hover:bg-[#6875B2] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#7886C7]">Play Sound</button>
+      <button onClick={() => stop()} className="p-2 border text-xs rounded cursor-pointer   text-white bg-[#7886C7] hover:bg-[#6875B2] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#7886C7]">Stop Sound</button>
     </div>
   );
 };

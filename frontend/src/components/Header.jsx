a758bc72f20e0d7641f64a4426766f5e97713206
stephipie <stephi.pietschmann@gmail.com>
2025-02-28 14:03:36 +0100

    change style within header component, rounded not needed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
   const hideNavigation = location.pathname === "/register" || location.pathname === "/";
 
   return (
-    <header className="bg-[#7886C7] flex w-full items-center justify-between rounded">
+    <header className="bg-[#7886C7] flex w-full items-center justify-between">
       {/* <h1 className="text-[#FFF2F2] py-4 mx-4">Holos App</h1> */}
       <img src={logo} alt="logo" className="App-logo" class="h-15 w-60 object-scale-down"/>
       

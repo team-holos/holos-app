3bdd6ac7bad7a993c6dd77f6ae45aa8179d8ce6b
stephipie <stephi.pietschmann@gmail.com>
2025-02-28 12:28:13 +0100

    add new file images to implement logo within header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Navigation from "./Navigation";
 import useAuthStore from "./store/authstore";
 import Logout from "./Logout";
+import logo from "../images/logo.png";
+
 function Header() {
   const {isLoggedIn} = useAuthStore();
   return (
     <header className="bg-[#7886C7] flex w-full items-center justify-between rounded">
-      <h1 className="text-[#FFF2F2] py-4 mx-4">Holos App</h1>
+      {/* <h1 className="text-[#FFF2F2] py-4 mx-4">Holos App</h1> */}
+      <img src={logo} alt="logo" className="App-logo" class="h-20 w-50 object-scale-down"/>
       
       <div className="flex items-center space-x-4">
         {isLoggedIn && <Navigation />}
